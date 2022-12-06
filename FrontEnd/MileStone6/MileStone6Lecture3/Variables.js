c006182b8d72f2d4e8daef00216237dcfaef27cf
harish kumar <harishkunar987@gmail.com>
2022-12-06 21:49:15 +0530

    upgrade

---
+++ FrontEnd/MileStone6/MileStone6Lecture3/Variables.js
@@ -0,0 +1,7 @@
+// 1 = here we learn about. that how we can declare (variables) in the javaScript.
+// here in javascript we use (var) for creating varaibles.
+var a = 10;
+// 2 = here in javascript (variable) does not depend on the the variables data-type.It working with its (value).means if we create variable (a).then this variable can  work with any data-type value .because in javascript variables work with there values only.
+a = "hello";
+// for printing  our coding in the javascript we use (console.log()) statement.under its brackets we define the variable that we want to print.
+console.log(a);
