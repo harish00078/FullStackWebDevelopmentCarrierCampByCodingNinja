4ea28a1d0f37b2b56f7d6b9c2c4ec27f88e90e61
harish kumar <harishkunar987@gmail.com>
2022-12-26 21:10:32 +0530

    upgrade

---
+++ FrontEnd/MileStone6/MileStone6Lecture7/CalculatorDemo/demo.js
@@ -0,0 +1,18 @@
+// under javascript we have function. that we use to get the (evaluation) of the values. acc to the operator that we gave to him:
+// name of the function is (eval()):
+
+// for example if we (multiples) the two values:we do that in two way's aslo:
+
+// 1 = first is that we can simple use the (eval) function with in the (console.log()) function:like this:
+console.log(eval('4 * 5'));
+
+// 2 = second is that we can create a two variables were we get the two values.and we create another one more variable were we provide the operator:
+var op1 = 4;
+var op2 = 5;
+var operator = '*';
+
+// and after that we create a another variables.were we use the eval function with these two variable values and also use the operator variable:Like this:
+var result = eval(op1 + " " + operator + " " +op2);
+// then we use the only last (result) variables in the print statement:
+console.log(result);
+
