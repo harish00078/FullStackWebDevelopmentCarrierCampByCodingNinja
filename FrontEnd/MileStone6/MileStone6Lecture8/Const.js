3506ebf96b24a15343f4f58dfa41a7dbfae38d97
harish kumar <harishkunar987@gmail.com>
2022-12-29 17:00:28 +0530

    upgrade

---
+++ FrontEnd/MileStone6/MileStone6Lecture8/Const.js
@@ -0,0 +1,15 @@
+// there are many ways to create or declare a variable in the javascript: one of them is:
+
+// 1 =  here we learn about (constant) = Const:
+// const = Is used to create a variable In the javscript.like same the other languages do:
+// but it  has some different properties acc to the other types of the variables declaration methods:
+// like:
+// 1 = It cannot be updated or re-declared into the scope:
+const a = 30;
+a = 40; // this will gave us error:
+
+
+// 2 = const can not be accessed without initialisation,as it can not be declared without initialisation:
+
+// const c; // this will gave us a error:
+// c = 30;
