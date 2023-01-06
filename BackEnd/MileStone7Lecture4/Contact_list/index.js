4567239639a51c6a71e76f5c210a5f51e3d88875
harish kumar <harishkunar987@gmail.com>
2023-01-06 21:11:52 +0530

    upgrade

--- BackEnd/MileStone7Lecture4/Contact_list/index.js
+++ BackEnd/MileStone7Lecture4/Contact_list/index.js
@@ -1,22 +1,45 @@
-const express = require('express');
+const express = require("express");
 
-
-const path = require('path');
+const path = require("path");
 
 const port = 8000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.set('views' , path.join(__dirname, 'view'));
+app.set("views", path.join(__dirname, "view"));
 
+// here we are creating a (contact-list). for our (Express) app:
+// we are creating a contact-list using a (array):
 
-app.get('/',function(req,res){
+var contactList = [
+  // under array we create (object-properties): which has the (keys) with there (values):
+  {
+    name: "harish",
+    phone: "9898989989"
+  },
+  {
+    name: "Tony Stark",
+    phone: "7979797979"
+  },
+  {
+    name: "coding Ninjas",
+    phone: "797786876868"
+  },
+];
 
-  return res.render('home',{title:"contact List"});
-})
+app.get("/List", function (req, res) {
 
+  return res.render("home", {
+
+    title: "contact List",
+
+    // here we create another (Object-property):which has the (key) with it's (value):
+    contact_List: contactList
+  });
+
+});
 
 app.listen(port, function (err) {
   if (err) {
