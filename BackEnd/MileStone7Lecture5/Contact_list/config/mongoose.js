7dc5c263b2a9fd50d6e0a1b760a8b32a4d19fe55
harish kumar <harishkunar987@gmail.com>
2023-01-16 21:26:56 +0530

    upgarde

--- BackEnd/MileStone7Lecture5/Contact_list/config/mongoose.js
+++ BackEnd/MileStone7Lecture5/Contact_list/config/mongoose.js
@@ -47,10 +47,11 @@ const db = mongoose.connection;
 
 // 1 => if it does not connected to the (mongodb) then we will get the (error) message on our console:
 // here we are using the antoher console function for printing the error in the console: and that function is (console.error.bind) function:
+// meaning of code = when we (on) or try to connect with the (mongoose): at that time if we get ('error').then we have to print the (error) in the (console).that we have given to him:
 db.on('error', console.error.bind(console,'error connecting to db'));
 
 // 2 => if it is connected  to the database then we have to print this funciton's statement:
-
+// meaning of code = if we (once) connect or (open) with the (mongoose).then we have to run this function and print the (statement) in the (console).that we have given to him:
 db.once('open', function(){
   console.log('successfully connected to the database');
 })
