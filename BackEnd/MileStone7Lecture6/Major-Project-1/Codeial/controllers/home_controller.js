da9750bfeb53c77a0abed8542c7800ed05c0b12f
harish kumar <harishpathania150@gmail.com>
2023-01-20 20:48:25 +0530

    working on Major Project -1

--- BackEnd/MileStone7Lecture6/Major-Project-1/Codeial/controllers/home_controller.js
+++ BackEnd/MileStone7Lecture6/Major-Project-1/Codeial/controllers/home_controller.js
@@ -1,6 +1,9 @@
-// here we are creating a (controller) or we can say (action):
-// for the (req):
+// here we are creating a (controller) or we can say (actions):
+// for the (requests) and providing  them a (responses):
 
+
+// here we create a (Controller):named as a (home):
+// and we also had to (export) controllers:so we can (use) them or we can say connect them with the particular (routes):
 module.exports.home = function(req,res){
 
   return res.end('<h1> Express is up for codial</h1>');
--- BackEnd/MileStone7Lecture6/Major-Project-1/Codeial/routes/index.js
+++ BackEnd/MileStone7Lecture6/Major-Project-1/Codeial/routes/index.js
@@ -3,25 +3,34 @@
 // we basically create a (Tree) like structure here:
 // (Tree): like structure means that we will basically get (req) from the (user): and acc to  that (req) basically create a another (file) for that (req). were we write all (response) for that particular (file) or (req):
 
-
 // => for creating  a  (tree) like structure of the (routes):
 // we have to use the Express's (router) module:
 
-
 // first we import (express) here:
 
-const express = require('express');
+const express = require("express");
 
 // know we can use the (router) module of the (express) framework:
 
 const router = express.Router();
 
+// here we are Importing the (controllers):that we have created in the (controller-folder):
+
+// here we Import the (home_controller) file from the (controller-folder):
+// under this (home_controller) file we basically write our (controllers) :
+// IMP = 
+const homeController = require("../controllers/home_controller");
+
 // for check that its working or not:
-console.log('router loaded');
+console.log("router loaded");
+
+
+// here we are connecting a particular (router) with the particular (controller):
+// for that we have to use the (get) function:
+// here we are doing that  if (router) gets this (req = '/') from the user:then we have him a this (home) controller in the (res):
+router.get('/', homeController.home);
+
 
 
 // after creating routes. know we have to (export) it for the (server-file):
 module.exports = router;
-
-
-
