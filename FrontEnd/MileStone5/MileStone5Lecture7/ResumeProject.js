7a711888eeea6eeb475042b2a396bbeed9ae330e
harish kumar <harishpathania150@gmail.com>
2023-01-28 17:22:57 +0530

    create smooth scroll

--- FrontEnd/MileStone5/MileStone5Lecture7/ResumeProject.js
+++ FrontEnd/MileStone5/MileStone5Lecture7/ResumeProject.js
@@ -81,6 +81,10 @@ var navMenuAnchorTags = document.querySelectorAll(".nav-menu a");
 // here we are printing:them so we can check that  we are getting the (correct) anchor-tag:
 console.log(navMenuAnchorTags);
 
+
+// here we creaate the (interval) variable globaly:so it can connect witht the (scroll) function: that we have created (separately):
+var interval;
+
 // here we create for-loop so we can go on every (anchor-tag):
 for (var i = 0; i < navMenuAnchorTags.length; i++) {
   // here we create a (smooth-scroll) (Event) on the (anchor-tag) that we are getting from the (navMenuAnchorTags) variable:
@@ -121,31 +125,94 @@ for (var i = 0; i < navMenuAnchorTags.length; i++) {
     // after getting the (section) or its (coordinates):know we can create (smooth-scroll) function on  it:
     // with the help of (setInterval) function:
 
-    // here we are creating (Interval) for the  (smooth-scrolling) of every (section) of the (webpage):
-    var Interval = setInterval(function () {
+    console.log(targetSection);
 
 
-      // we need to create anther variable: were we are getting the (coordinates) of that (content) or we can say that (section):
-      // basically we are using the (coordinates) of the (sections): so we can create (smooth-scrolL) on the (sections):
-      // for getting the (coordinates) of that section: we need to use the (getBoundingClientRect) function:
 
-      // IMP = we are  checking the (coordinates) of the (section): here so when ever we (scroll) on (webpage): after that we have to (check) its (coordinates): to see that we (reach) the (top) dimensions to (zero) or not:
-      var targetSectionCoordinates = targetSection.getBoundingClientRect();
 
-      // we are using the (coordinates) of the (section): to tell the (interval): that were we have to (stop) the (interval) or clear the (interval):
 
-      // IMP = when we are at (top) of the (webpage):the (top) will have its (dimensions):
-      // but we reach the (particular) (section) of the (webpage): then that (top) dimensions will become (zero):
-      // so we have to clear or stop our (interval): when the (top) coordinate will get the (dimensions) of (zero):
+    // here we create (scroll) function: for particular webpage:
 
-      if (targetSectionCoordinates.top <= 0) {
-        clearInterval(Interval);
-        return;
-      }
+    // // here we create the (smooth-scroll) function (under) the (particular-webpage):
+    // // we can aslo create it (separately):so we can use it for anyother (webpages)also : if we have a (multiple) webpages in our website:
 
-      window.scrollBy(0, 50);
+    // // here we are creating (Interval) for the  (smooth-scrolling) of every (section) of the (webpage):
+    // var Interval = setInterval(function () {
+    //   // we need to create anther variable: were we are getting the (coordinates) of that (content) or we can say that (section):
+    //   // basically we are using the (coordinates) of the (sections): so we can create (smooth-scrolL) on the (sections):
+    //   // for getting the (coordinates) of that section: we need to use the (getBoundingClientRect) function:
 
-      // here we are giving the (50 = millisecond) time to our (interval) function:
-    }, 30);
+    //   // IMP = we are  checking the (coordinates) of the (section): here so when ever we (scroll) on (webpage): after that we have to (check) its (coordinates): to see that we (reach) the (top) dimensions to (zero) or not:
+    //   var targetSectionCoordinates = targetSection.getBoundingClientRect();
+
+    //   // we are using the (coordinates) of the (section): to tell the (interval): that were we have to (stop) the (interval) or clear the (interval):
+
+    //   // IMP = when we are at (top) of the (webpage):the (top) will have its (dimensions):
+    //   // but we reach the (particular) (section) of the (webpage): then that (top) dimensions will become (zero):
+    //   // so we have to clear or stop our (interval): when the (top) coordinate will get the (dimensions) of (zero):
+
+    //   if (targetSectionCoordinates.top <= 0) {
+    //     clearInterval(Interval);
+    //     return;
+    //   }
+
+    //   window.scrollBy(0, 50);
+
+    //   // here we are giving the (50 = millisecond) time to our (interval) function:
+    // }, 30);
+
+
+
+
+    // here we are using (scroll) function:that we have created separately:so we can use it another web-pages as well:
+    // here we are using the (scrollVertically) function:that we have created separately:
+    // we aslo have to provide  him the (targetSection): so it stop on the (right) or we can say on the (particular)  section of the (webpage):
+    // we create (interval) variable globaly:so can use the (scroll) function that we have create (separately):
+
+    // interval = setInterval(scrollVertically,20,targetSection);
+
+    // we can write the  function in this way also:
+    interval = setInterval(function(){
+      scrollVertically(targetSection);
+    },20);
+
+
+
+
   });
+
+
+
+}
+
+
+
+
+// here we are creating a (scroll) function separately: so we can use it:
+// for another-pages as well: if we have another web-pages in our website:
+
+
+// here we are creating (Interval) for the  (smooth-scrolling) of every (section) of the (webpage):
+// we also have to provide him the (targetSection): so it will stop on the (right) or we can say on the (particular) section of the (webpage):
+function scrollVertically(targetSection) {
+  // we need to create anther variable: were we are getting the (coordinates) of that (content) or we can say that (section):
+  // basically we are using the (coordinates) of the (sections): so we can create (smooth-scrolL) on the (sections):
+  // for getting the (coordinates) of that section: we need to use the (getBoundingClientRect) function:
+
+  // IMP = we are  checking the (coordinates) of the (section): here so when ever we (scroll) on (webpage): after that we have to (check) its (coordinates): to see that we (reach) the (top) dimensions to (zero) or not:
+  var targetSectionCoordinates = targetSection.getBoundingClientRect();
+
+  // we are using the (coordinates) of the (section): to tell the (interval): that were we have to (stop) the (interval) or clear the (interval):
+
+  // IMP = when we are at (top) of the (webpage):the (top) will have its (dimensions):
+  // but we reach the (particular) (section) of the (webpage): then that (top) dimensions will become (zero):
+  // so we have to clear or stop our (interval): when the (top) coordinate will get the (dimensions) of (zero):
+
+  if (targetSectionCoordinates.top <= 0) {
+    clearInterval(Interval);
+    return;
+  }
+
+  window.scrollBy(0, 50);
+
 }
