d2b82f81a9854294fd62fd0cf87f7336e8435929
harish kumar <harishpathania150@gmail.com>
2023-02-27 22:17:35 +0530

    learn about the nodemailer

--- BackEnd/MileStone8Lecture19/ParallelJob+Mailer/codeial/config/nodemailer.js
+++ BackEnd/MileStone8Lecture19/ParallelJob+Mailer/codeial/config/nodemailer.js
@@ -79,7 +79,7 @@ let renderTemplate = (data,relativePath) =>{
 
       // template = here template is basically the (structure) of the (email):which also has a (user) data in it:
 
-      // if we have (err)
+      // if we have (err):while rendering the (ejs) template:
       if(err){
 
         console.log('error in rendering template');
