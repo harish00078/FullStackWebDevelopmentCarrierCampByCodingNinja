183a67d05a3f06f58de2d45fc3444b8bec0de5fe
harish kumar <harishpathania150@gmail.com>
2023-03-03 12:31:28 +0530

    update

---
+++ FrontEnd/MileStone6/MileStone6Lecture10/objectVsObject.js
@@ -0,0 +1,13 @@
+// here we learn about that:what is the difference between (object) and (Object):
+
+
+// => Object:
+// basically the (Object) which has the (capital) letter (O):is the constructor function:
+// and this  constructor function is basically used to create (object) in the javascript:
+
+
+// => object:
+// this (object):which has the small letter of (o):is basically the data type of the (javascript):and also it is non-primitive datatype:
+// this object is basically the collection of the (key,value) pairs:
+// and these key-value pairs or objects we can say are created with the help of (Object) constructor function:
+
