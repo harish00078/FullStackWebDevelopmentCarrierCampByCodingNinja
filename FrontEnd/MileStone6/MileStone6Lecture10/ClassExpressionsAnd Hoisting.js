7439cbd15941d680cd65496a2019949f1f220f4c
harish kumar <harishpathania150@gmail.com>
2023-03-04 14:41:15 +0530

    learn about ClassExpressionsAnd Hoisting

--- FrontEnd/MileStone6/MileStone6Lecture10/ClassExpressionsAnd Hoisting.js
+++ FrontEnd/MileStone6/MileStone6Lecture10/ClassExpressionsAnd Hoisting.js
@@ -1,11 +1,13 @@
 // here we learn about the (class) expression and (hoisting):
 
 //  if we try to create the vehicle(object):before we declare the class for that (object):
-var truck = new vehicle(12,34624654);
+
+// var truck = new vehicle(12,34624654);
 
 // and if we try to print it:then this will not we printed:because (class) function are not (hoisted) in the javascript:
 // (hoisted) means the function:which we can be accessable in anywhere of the (file):were that function has been created:
-console.log(truck);
+
+// console.log(truck);
 
 
 // here we are creating the function: or we can say (constructor) funtion:with the help of (class) keyword:
@@ -45,3 +47,31 @@ var car = new vehicle(8,35234512354523);
 console.log(car);
 
 console.log(vehicle.prototype);
+
+
+
+
+//  if we try to create the student(object):before we declare the class for that (object):then it will not be created:even if create the class with thee  help of (Expression):it will not able to create the (object):
+// because class declaration are not (hoisted):
+var harish = new student(12,8667888877);
+// and if we try to print it:then this will not we printed:because (class) function are not (hoisted) in the javascript:
+// even if we create the (class) with the help of (Expression):
+// (hoisted) means the function:which we can be accessable in anywhere of the (file):were that function has been created:
+
+console.log(harish);
+
+
+
+// if we create the class:with the help of (expression):or we can say with the help of (variable):
+// Expression = JavaScript’s expression is a valid set of literals, variables, operators, and expressions that evaluate a single value that is an expression. This single value can be a number, a string, or a logical value depending on the expression.
+
+var student = class{
+
+  constructor(rollNo,Phone_no){
+
+    this.rollNo = rollNo;
+    this.Phone_no = Phone_no;
+  
+  }
+
+}
