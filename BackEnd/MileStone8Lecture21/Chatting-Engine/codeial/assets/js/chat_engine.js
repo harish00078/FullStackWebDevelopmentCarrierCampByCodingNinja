624c17e7457c50eb436a72cbcfedd5c6b4b9248b
harish kumar <harishpathania150@gmail.com>
2023-03-12 20:46:15 +0530

    learn about chat_engine with sockets

--- BackEnd/MileStone8Lecture21/Chatting-Engine/codeial/assets/js/chat_engine.js
+++ BackEnd/MileStone8Lecture21/Chatting-Engine/codeial/assets/js/chat_engine.js
@@ -1,1 +1,47 @@
 // here we are setting-up the (socket.io) for the (client)side as well:
+
+//  for the (client)side:we create the socket function:with the help of the (class) objects:
+
+class ChatEngine{
+
+  // class constructor will take two things as arguments:
+  // In first argument it will have the (id) of the (chat-box):
+  // In second argument it will have the (emial-ID) of the (user):to know that which (user) or we can say who initiated the socket connection:
+  // and also to  knows that: who is sending the (message):
+  constructor(chatBoxId, userEmail){
+
+    this.chatBox = $(`#${chatBoxId}`);
+    this.userEmail = userEmail;
+
+
+    // here we connect the (client)side with the (socket) server:with the help of the inbuilt (io) function:which is provided by the (socket) CdnJs file:
+    this.socket = io.connect('http://localhost:5000');
+
+
+    // here we are connecting with the (client) side function:with the socket (connection-handler) that we have created:
+
+    // we only connect the (client) side with the socket (connection-handler):if the (client) side (constructor) will have the (email) of the (user) with in it:
+    if(this.userEmail){
+      this.connectionHandler();
+    }
+
+
+  }
+
+  // here we create the (socket) connection-handler for the (client) side:with the (server) side:
+  connectionHandler(){
+    // this socket or socket connection handler:will have two function in it:
+    // first (function) is the (on):this basically tells that we are getting the (messsage):
+    // second (function) is the (amit):this basically tells that we are sending the (message):
+
+    // when socket  (on) function get triggered:then we will first connect with the (client):who trying to  sending us the (message): with the help of the (connect) Event:
+    this.socket.on('connect', function(){
+
+      console.log('connection established using sockets...!');
+
+    });
+
+  }
+
+
+}
--- BackEnd/MileStone8Lecture21/Chatting-Engine/codeial/config/chat_sockets.js
+++ BackEnd/MileStone8Lecture21/Chatting-Engine/codeial/config/chat_sockets.js
@@ -2,11 +2,37 @@
 // here we are setting it up for the (server) side:
 // its also set up in the (client) side:because web-sockets established the communication in (both-ways):
 
-
 // here we are creating the (socket.io) function for the chat-Box:from the server-side:
 // here (socketServer) argument is basically the (chatServer) that we have created in our (index.js) file:or we can say in our (server-file):
-module.exports.chatSockets = function(socketServer){
+module.exports.chatSockets = function (socketServer) {
+
+  // here we are giving the (socket.io) library or the (port) that we have created in the (server-file):to the (io) variable: or we can say to the socket-function of the (server-side):that we have created: 
+  let io = require("socket.io")(socketServer);
+
+  // socket will have two function with in it: In the (server-side) as well:
+  // first (function) is the (on):this basically tells that we are getting the (messsage):
+  // second (function) is the (amit):this basically tells that we are sending the (message):
+
+  // when socket  (on) function get triggered:then we will first connect with the (server):who trying to  sending us the (message): with the help of the (connection) Event:
+  io.sockets.on('connection', function(socket){
+
+
+    console.log('new connection received',socket.id);
+
+    // if we create the (disconnected) socket Event:
+    socket.on('disconnect', function(){
+
+      console.log('socket connection disconnected ');
+
+    });
 
 
 
-}
+
+
+  });
+
+
+
+
+};
