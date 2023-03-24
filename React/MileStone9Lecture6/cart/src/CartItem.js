e3e417e46fda163c5ebe656e7e8feea1b7c4b7e2
harish kumar <harishpathania150@gmail.com>
2023-03-24 12:28:29 +0530

    learn about  setState in React

--- React/MileStone9Lecture6/cart/src/CartItem.js
+++ React/MileStone9Lecture6/cart/src/CartItem.js
@@ -11,6 +11,8 @@ class CartItem extends React.Component {
     };
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+
+
   increaseQuantity = () => {
 
 
@@ -42,6 +44,40 @@ class CartItem extends React.Component {
 
     // })
 
+    // if we try to increase the (quantity) of the (product):by more than one at a particular time:
+    // and for that we create the (number) of (setState) functions:for increasing the (quantity) of the (product) by multiple times:with the help of the (first-form) of the (setState) function:
+    // IMP = then this will not work:because of the (batching) system in the (react):
+    // Batching:=> this batching system basically convert the same type of the functions into the one set of (function):so that's why those multiple functions get executed by only  once:and because of that the (quantity) of the (product) is only  incremented by once:
+    // how react will able to do that (batching):because of the (shallow) rendering feature:
+    // shallow rendering: Shallow rendering basically lets you render a component “one level deep” and assert facts about what its render method returns, without worrying about the behavior of child components, which are not instantiated or rendered. This does not require a DOM.
+
+    // In basic words: shallow rendering feature:basically get all the same type of functions:and it will only render the (last-function) from amoung all of them:
+
+    // this.setState({
+
+    //   // here we are changing the  (quantity) in the (state) object:
+    //   qty:this.state.qty + 1
+
+    // });
+
+    // this.setState({
+
+    //   // here we are changing the  (quantity) in the (state) object:
+    //   qty:this.state.qty + 1
+
+    // });    
+
+    // this.setState({
+
+    //   // here we are changing the  (quantity) in the (state) object:
+    //   qty:this.state.qty + 5
+
+    // });
+
+
+    
+
+
 
 
     // setState form-2: In second form we can use this function:by simply giving the (callback - function) to it:
@@ -58,11 +94,69 @@ class CartItem extends React.Component {
 
     });
 
+    // but if we increase the quantity of the Product:by multiple times with the help of the (second-form) of the setState function:
+    // then it will successfully able to increase the quantity of the Product by multiple times:
+    // but it will also happen in the (Batching) process:means all those multiple functions only get rendered by once:
+    // but In second-form of setState function:we will able to increase the quantity of the Product by multiple times:even if we have (Batching) process happen:
+    
+    this.setState((prevState) => {
+
+      // here we are giving the change value to the state object:so that we can change the value of the quantity key in the state object:
+      return{
+
+        qty: prevState.qty + 1
+      }
+
+    });    
+    
+    this.setState((prevState) => {
+
+      // here we are giving the change value to the state object:so that we can change the value of the quantity key in the state object:
+      return{
+
+        qty: prevState.qty + 1
+      }
+
+    });
+
+
 
     console.log("this", this.state);
 
 
   };
+
+
+  // here we are creating the (decreaseQuantity) function:
+  // for the (decrease) button of the (cartitem):
+  decreaseQuantity = () => { 
+
+    // here we put the (check) quantity feature:
+    // so that we did not convert the  quantity of the (product) into the (-1):while decreasing it:
+
+    const {qty} = this.state;
+
+    if(qty === 0){
+
+      return ;
+
+    }
+
+    this.setState((prevState) => {
+
+      // here we are giving the change value to the state object:so that we can change the value of the quantity key in the state object:
+      return{
+
+        qty: prevState.qty - 1
+      }
+
+    });
+
+
+    console.log("this", this.state);
+
+  }
+
 
   render() {
     const { price, title, qty } = this.state;
@@ -87,6 +181,8 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/9632/9632763.png"
+              onClick={this.decreaseQuantity}
+
             />
             <img
               alt="delete"
