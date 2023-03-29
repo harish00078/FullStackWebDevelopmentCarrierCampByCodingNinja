942e24806b03f20129b47894695986bc599e17f9
harish kumar <harishpathania150@gmail.com>
2023-03-29 13:29:48 +0530

    cartitem React app

--- React/MileStone9Lecture7/Mini-Project-Finishing-up/cart/src/Cart.js
+++ React/MileStone9Lecture7/Mini-Project-Finishing-up/cart/src/Cart.js
@@ -5,20 +5,15 @@ import React from "react";
 // so for having (cart-items) in the (cart) file:we have to import the (cart-item) file:
 // through which we can create (list) of cart-items in the (Cart) file:
 
-import CartItem from './CartItem';
-
+import CartItem from "./CartItem";
 
 class Cart extends React.Component {
-
-  render(){
-
+  render() {
     // here we create (array):which will have (list) of (numbers) in it:
-    const arr = [1,2,3,4,5,];
-    return(
-
+    // const arr = [1, 2, 3, 4, 5];
+    return (
       // here we are giving the (list) of (cart-items) to the (cart) file:
-      <div className="cart"> 
-
+      <div className="cart">
         {/* here we are rendering the (list) of (numbers) of the (array) in the browser: */}
 
         {/* {arr} */}
@@ -26,27 +21,28 @@ class Cart extends React.Component {
         {/* we can also (loop) or (iterate) over the each (item) of the (ARRAY):
         IMP => and add some additional (features) or (values) in it: while we are (iterating) over them :  */}
         {/* for doing that we have to use the (map) method:which will help us to do that: */}
-        
+
         {/*  // here (item) argument:will have the (itams) of the (array): one by one */}
-        {arr.map((item) => {
+
+        {/* {arr.map((item) => {
 
          
           // and we will add some (values) on them (one by one):
 
           return item + 5
 
-        })}
+        })} */}
 
         <CartItem />
-        <CartItem />
-        <CartItem />
-
 
 
 
       </div>
-    )
-    
+
+
+    );
+
+
 
   }
 
