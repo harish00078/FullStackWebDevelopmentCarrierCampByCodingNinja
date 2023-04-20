981109ff0fbdb4b23819208132a56b2177b7a9d9
harish kumar <harishpathania150@gmail.com>
2023-04-20 14:54:32 +0530

    learn about firebase in React App

--- React/MileStone9Lecture9/FireBaseMiniProjectExtend-2/cart/src/App.js
+++ React/MileStone9Lecture9/FireBaseMiniProjectExtend-2/cart/src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./Navbar";
 // here we are importing the firebase:
 import firebase from "firebase/app";
 
-import 'firebase/firestore';
 
 class App extends React.Component {
 
@@ -14,6 +13,10 @@ class App extends React.Component {
     super();
 
     this.state = {
+
+      products:[],
+
+
 
       // here we are giving the (products) to our (state-object):from the (firebase) database:
       // for getting the data from the database:and put it into on the (component):for that we have to use the (ComponentDidmount) function of the component lifecycle:
@@ -41,9 +44,15 @@ class App extends React.Component {
       //     id: 3
       //   }
       // ]
+
+
     };
+
+
     // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
+
+
   }
 
   // here we are using the (componentDidMount) method or function:so that we can get the data from the database: and put it on our component:
@@ -59,15 +68,28 @@ class App extends React.Component {
     
     // => second = we have to define that but kind of (collection) we are giving to this component:from the firebase's  database: here we want to gave the (products) collection to this component:
 
-    // => third = we 
+    // => third = we need to use the (get) method:this (get) method basically get the data from the (collection) of the (database):that we have given to him:
+    // IMP -> this (get) method basically return us the (promise):In the form of Object type Task (QuerySnapshot):
 
+    // In firestore we have specific type of mechanism for reading the data once. To read a single document, we can use DocumentReference’s get() method that returns a Task (<DocumentSnapshot>): while reading multiple documents from a collection or Query, we can use Firestore Query’s get() method that returns an object of type Task (<QuerySnapshot>). Both methods read the data only once.
+
+    // => fourth = know after getting the data from the Firestore database:with the help of the (get) method:we have to use that (data) in our component:
+    // so for that we have to use the (then) function or method:because the data that we are getting from the (get) method is in the form of (promise):
+
+    // Imp => (then) method is basically a callback function of the (promise):when ever (promise) get resolved :it will attaches a callback function for the (fulfilled/success) and (rejection) of the (promise):
+    
+    // (then) = JavaScript Promise then () method is called whenever a promise is resolved. It takes data from the resolved promise. It can take up to two arguments which are (callback) functions for the (fulfilled/success) and rejected cases respectively. Just like the catch () method it also returns a Promise so it is used to chain Promises.
+
+    // V.IMP => we are also giving the (querysnapshot) object to our (then) method or function:because that (object) will have the (data) of the (collection) which we want to (get or fetch) from our database:we are getting this (object) or (data) with the help of (get) method:
 
     firebase
+
+
       .firestore()
       .collection("products")
       .get()
       .then((snapshot) => {
-        console.log("database-data".snapshot);
+        console.log(snapshot);
       });
 
   }
