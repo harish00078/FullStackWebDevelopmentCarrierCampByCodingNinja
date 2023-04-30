20d3d35272ed69c392fcf9bb59217e6ef5899111
harish kumar <harishpathania150@gmail.com>
2023-04-30 13:53:59 +0530

    learn about ReactHooks

---
+++ React/MileStone9Lecture11/ReactHooks/learnreacthooksforlifecyclemethods/src/App.js
@@ -0,0 +1,12 @@
+// here we learn about that:how can we use component-`life-cycle methods in the (function) component:with the help of (React-hooks):
+function App() {
+  return (
+    <div className="App">
+ 
+    </div>
+  );
+}
+
+export default App;
+
+
