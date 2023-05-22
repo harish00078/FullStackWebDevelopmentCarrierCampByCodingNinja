fafb44770a9dbe7b8d719b8f20132163e43772fc
harish kumar <harishpathania150@gmail.com>
2023-05-22 15:58:15 +0530

    creating hooks blog app with react

--- React/MileStone9Lecture12/react-hooks-blog/src/components/Home.js
+++ React/MileStone9Lecture12/react-hooks-blog/src/components/Home.js
@@ -6,6 +6,9 @@ import { useEffect, useState } from "react";
 // here we import the (firestore):
 import {firestore} from '../firebase';
 
+// here we import the (Link) tag from the (react-router-dom) library:
+import {Link} from 'react-router-dom';
+
 
 
 function Home(){
@@ -32,18 +35,45 @@ function Home(){
     // IMP => we will gave the (callback) function to the (then) function:
     // and To that (callback) function:we will gave the (snapshot) argument:and that (snapshot) argument:will have the (snap) or we can say (data) of our database:
 
-    firestore.collection('post').get().then((snapshot) => {
-      console.log('data',snapshot);
+    firestore.collection('posts').get().then((snapshot) => {
 
       // know for getting the (data):from the (snapshot) argument:
       // we need to go into its (docs) section:because In that (docs) section:we will have our (data) of (posts):
       // IMP => and for getting the (data) of every (post) from that  (snapshot's) (docs) section:we need to use the (map) function on that (docs) section of  (snapshot) argument:
 
-    })
+      const posts = snapshot.docs.map((doc) => {
+
+        // IMP => and from this (docs) section: we need to get or we can say retrun the two things:
+        // first is the (doc-id):we need to get it separately:because it does not get along with the (data):
+        // second is the (doc-data): where we have our (posts-data):we will get all the (data) which were present in the (docs) section:with the help of the (spread) operator:
+        return{
+          id:doc.id,
+          ...doc.data()
+        };
+
+
+      });
+
+
+      console.log('posts',posts);
+
+
+      // after getting the (posts-data):from the (database):
+      // we need to gave this data to the (setPosts) function of (useState) method:
+      // so that we can show this (posts) data on the browser:
+
+      setPosts(posts);
+
+
+
+
+    });
+
+   
 
 
     // here we add the empty array in (useEffect) hook: so that (useEffect) hook does not run again and again on the every-time refresh of the (application):
-  },[]);
+  }, []);
 
 
 
@@ -52,7 +82,9 @@ function Home(){
     <h1>Tech Blog</h1>
     {/* here we gave author-name to the blog: */}
 
-    <div id="blog-by">Harish</div>
+    <div id="blog-by">
+      <h2>Harish</h2>
+      </div>
 
     {/* here we are displaying the (posts):
     =>  for that we need to use the for-loop:so that we can display every (post):
@@ -73,11 +105,11 @@ function Home(){
         => and we also need to pass the (post-id):In that (urL):so that we can get the (detials) of the particular (post): */
         
         }
-        <link to={`/post/${post.id}`}>
+        <Link to={`/post/${post.id}`}>
           {/* post (title) will become our (link): */}
           <h3>{post.title}</h3>
 
-        </link>
+        </Link>
 
         {/* we also add the (subtitle) of the (post):outside the linK:
         => so that user can have more clreaty related to the (post): */}
