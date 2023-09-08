09d5b4db1343d106d5cfb421b0872cb95cfa569b
harish kumar <harishpathania150@gmail.com>
2023-09-08 22:03:43 +0530

    build the social media react application

--- React/MileStone9Lecture14/Major-Project/codeial-react-app/api/index.js
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/api/index.js
@@ -1,7 +1,27 @@
 // here we are creating  the (customFetch) function:
 // so that we did not have to write the (fetch) function.again and again for the every function which we will create to get the data from the APIs related to the particular compomemt element of our application:
 // with the help of this (customFetch) function.we did not have to write the (try) and (catch) method again and again as well. for the every function which we have created to get the data of particular application component element: 
-const customFetch = () => {
+
+// IMP = we also create our (customfetch) function:with the help of (async-await) method:
+// IMp = In the custom-fetch function.we will put the (url) and (object or config-ob ject) as the argument.this (config-object) argument will have two things or keys:
+// 1 => (body) = body is basically a (user) data.like (email or password) or we can say the other (credentials or data) of (user).which we want to send when we are calling the (api) which will require the (user) data.
+// 2 => (customConfig) = 
+
+const customFetch = async (url,{body}) => {
+
+  // for (fetching) the data from the back-end (API). we will basically use the (try) and (catch) method. so that our application did not get (crashed).while we are fetching the data from the backend (api):
+  
+  try{
+
+    // here we make the fetch request the (api):
+    // IMP => we are also using the (await) function.while fetching the data from the (api):
+    // In the fetch function.we will put the (url) and (config) argrument.because we want to fetch the data  from the backend or backend-api's.acc to the (url) and (config) be pass to this (customfetch) function.because here we need to fetch the data from the different backend (api's) acc to our application components.
+    const response = await fetch(url,config);
+
+
+  }catch(error){
+    console.log('error');
+  }
 
 };
 
