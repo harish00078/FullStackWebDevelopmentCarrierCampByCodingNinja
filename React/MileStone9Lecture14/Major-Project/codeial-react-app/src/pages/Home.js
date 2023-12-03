b7901caa061f1c6cc73ae499028aa65d8cce6a7f
harish kumar <harishpathania150@gmail.com>
2023-12-03 19:16:31 +0530

    build the social media app using react and backend api's

---
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Home.js
@@ -0,0 +1,20 @@
+// here we are creating the home-component for our application:
+
+// we are creating the home page or component through  (Function) component type:
+// so for creating the (home) page we can use the (arrow) function:instead of using casual function:
+// and we can also use the (Const) variable-type on that arrow function:
+
+const Home = () => {
+  // under this arrow function.we will write the (html) for our (home-page):
+  // and return it from this function.
+  // so that we can export it from this file: and use it in our (application):
+
+  return(
+    
+  );
+
+
+};
+
+// here we are exporting the (home-page) component:
+export default Home;
