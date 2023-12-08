8bd75523b96f78a789045831584de64636f1da90
harish kumar <harishpathania150@gmail.com>
2023-12-08 20:09:57 +0530

    build the social media app with the help of react

--- React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Home.js
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Home.js
@@ -1,4 +1,7 @@
 // here we are creating the home-component for our application:
+// here we are importing the (css-module) which we have created particularly for this (component) of our application:
+import styles from '../styles/home.module.css'
+
 
 // we are creating the home page or component through  (Function) component type:
 // so for creating the (home) page we can use the (arrow) function:instead of using casual function:
@@ -14,30 +17,35 @@ export const Home = () => {
   // so that we can export it from this file: and use it in our (application):
 
   return (
-    <div className="posts-list">
+    // IMP => here we are using the (css-module):with the help of creating (javascript) object in the (html) elements:
+    // and under that object we will define the particular value of the (css-element):
+    // which we wanna gave that to the particular element of the (html):
+    // IMP => after creating the (object) we need to connect that (object) with the (className) method of the react:
+    // V.IMP => so that react knows we are giving the (style) to our (html) elements through this (javascirpt-object):
+    <div className={styles.postList}>
       {/* post-wrapper is basically repersents the particular (post) */}
-      <div className="post-wrapper">
-        <div className="post-header">
-          <div className="post-avatar">
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
             <img
-              src="https://cdn-icons-png.flaticon.com/128/11589/11589468.png"
+              src="https://cdn-icons-png.flaticon.com/128/13135/13135440.png"
               alt="user-pic"
             />
             <div>
-              <span className="post-author">harish</span>
-              <span className="post-time">a minute ago</span>
+              <span className={styles.postAuthor}>harish</span>
+              <span className={styles.postTime}>a minute ago</span>
             </div>
           </div>
-          <div className="post-content">Post Content</div>
-          <div className="post-actions">
-            <div className="post-like">
+          <div className={styles.postContent}>Post Content</div>
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
               <img
                 src="https://cdn-icons-png.flaticon.com/128/126/126473.png"
                 alt="likes-icon"
               />
               <span>5</span>
             </div>
-            <div className="post-comments-icon">
+            <div className={styles.postCommentsIcon}>
               <img
                 src="https://cdn-icons-png.flaticon.com/128/1380/1380338.png"
                 alt="comments-icon"
@@ -45,18 +53,18 @@ export const Home = () => {
               <span>2</span>
             </div>
           </div>
-          <div className="post-comment-box">
+          <div className={styles.postCommentBox}>
             <input placeholder="start typing a comment" />
           </div>
 
-          <div className="post-comments-list">
-            <div className="post-comments-item">
-              <div className="post-comment-header">
-                <span className="post-comment-author">Bill</span>
-                <span className="post-comment-time">a minute ago</span>
-                <span className="post-comment-likes">22</span>
+          <div className={styles.postCommentList}>
+            <div className={styles.postCommentItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
               </div>
-              <div className="post-comment-content">random comment</div>
+              <div className={styles.postCommentContent }>random comment</div>
             </div>
           </div>
         </div>
