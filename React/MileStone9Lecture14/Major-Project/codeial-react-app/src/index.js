a3c1b6208ef427f38215717d6bb5aa05478a73e9
harish kumar <harishpathania150@gmail.com>
2023-12-16 20:35:20 +0530

    social-app with react and backend-api's

--- React/MileStone9Lecture14/Major-Project/codeial-react-app/src/index.js
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/src/index.js
@@ -2,13 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { App } from "./components/index";
+
+// here we are importing our notification library:
+// In our application's main-file named as (react-toast-notifications):
+// IMP => we need to import the particular (package) from this library:
+// which is named as (ToastProvider):
+import { ToastProvider } from "react-toast-notifications";
+
 // import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Router> */}
-    <App />
+    {/* we will wrap up our hole application:
+        => under this (ToastProvider) package of the (react-toast-notifications) library:
+        => so that we can use it.In any where of our application */}
+        {/* V.IMP => we aslo have to gave some properties to it:
+        => first is the (autoDismiss):so that notification will automatically get removed after showing:it will also have a value in the form of (true) and (false):and by default its value is (true):
+        => second is the (autoDismissTimeout).we also have to gave the time to the notification.To tell it that after how much it has to remove from the web-page:we gave him the time-data in the form of (miliseconds)
+        => third is the (placement):we also need to place the notifications.like where we want them to pop-up on the web-page:*/}
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <App />
+    </ToastProvider>
     {/* </Router> */}
   </React.StrictMode>
 );
--- React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Login.js
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Login.js
@@ -1,9 +1,25 @@
 // here we are creating the Login page:
 
 // here we are importing the style module:which we have created particularly created for this component:
+import { useState } from "react";
 import styles from "../styles/login.module.css";
 
 const Login = () => {
+  // we are using the (useState) hook:To get the data from the (form):
+  // and gave that data to the component elements:which will send that data to the (server) for authentication the user with the help of that data:
+
+  // first (useState) hook:we create for the (email) data:
+  const [email, setEmail] = useState("");
+  // second (useState) hook: we create for the (password) data:
+  const [password, setPassword] = useState("");
+
+  // IMP = third => we also need to maintain the (data) of the user's (login) button:
+  // so that user did not click that login button again and again:
+  // IMP = until its get the information from the server: related to its first login-request to the server:
+  // IMP = By default its (false):means user can use that button to send request to the server:
+  // but when request get send to the server:then that button get disabled.until user did not get the conformation related for its first-request to the server:
+  const [loggingIn, setLoggingIn] = useState(false);
+
   // under the Login-page:we will create the (Form):
   // through which we will get the input from the user related to there user-id and password:
   // and gave that data back to the server.TO check that this user is authorized user of the application or not:
@@ -16,12 +32,49 @@ const Login = () => {
         =>under that div-tag.we basically use the input-tags to get (data) from user related to there login credentials: */}
       {/* this div's input-tag get the e-mail input from the user: */}
       <div className={styles.field}>
-        {/* we are using the three propeties of the input-tag:
+        {/* we are using the propeties of the input-tag:
         => first is (type) of the input-tag.
         => second we use the (placeholder) in it.To gave some repersentation value to the input-tag.so that user knows what kind of input they need to put in this particular input-tag
         => third is the (required) ristriction property:so that user did not leave this input-tag empty: */}
 
-        <input type="email" placeholder="Email" required />
+        {/* V.IMP = here we are using the another property of the input-tag:
+        => through which we will gave the (initial-value) to (value) the email (input-tag): 
+        => IMP = for that we are using the (value) property of the (input-tag):
+        => and that (value) property. will get the (value) for it. from the (email) variable of the useState hook*/}
+
+        {/* V.IMP = we are also using the (event-handler) on this input-tag:
+        => so that we can get its (data) or (value).when it get triggered by the user.
+        =< by simply clicking on of its (form) button or submit-button:
+        V.IMP = after getting its data. we will send that data to our (setEmail) variable of the useState hook:
+        = which will pass that data to the (email) variable of (userState) hook:
+        => and from that varaible.we will send the user-email data to the (server).for authenticating the (user): */}
+        {/* V.V.IMP = we are using the (onchange) event-handler.
+        => 1 =  we need to add that (onChange) event-handler on the (input-tag).
+        => 2 =  so that when ever the (input-tag) get triggered:
+        => 3 =  then this event-handler will collect it (data):
+        // after getting the data of input-tag in the event-handler:
+        V.V.IMP = we will create the arrow-function in the (onchange) event-handler:
+      => so that we can get the event-data of the input-tag from the (onchange) event-handler.
+      => we will pass the event-handler data to the function as a (arguement).by simply adding (e) in the argument:
+      => this (e) repersent the event-handler.
+      => know we need to get the (data) from this (e) or event-handler.
+      => for that we need to use the property of the (event-handler).which will gave us the (data) or (value) of the event-handler:
+      => and that property name is (e.target.value):
+      // IMp = after getting the value or data of the input-tag.with the help of (event-handler):
+      => we have to gave that data to the (setEmail) state variable of the (useState) hook:
+      => and that (setEmail) state variable of useState hook.which is basically a function itself.
+      => IMP = that function basically used to change value of the other-ariable of the (useState) hook.which will have the initial or starting value related to the element:
+      => and when that initail-value holder variable get the value from the function-variable:
+      => then we will use that (initail-value) holder varaible :To send the (data) of the (users) credentials to the server.through which user  are try to authenticate in the application: 
+      */}
+
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         {/* this div's input tag basically get the password input from the user: */}
       </div>
       <div className={styles.field}>
@@ -30,12 +83,27 @@ const Login = () => {
         => second we use the (placeholder) in it.To gave some repersentation value to the input-tag.so that user knows what kind of input they need to put in this particular input-tag
         => third is the (required) ristriction property:so that user did not leave this input-tag empty: */}
 
-        <input type="password" placeholder="Password" required />
+        {/* V.V.IMP = same thing we need to do.
+        => for collecting the value of password from its input-tag: 
+        => the way we did in collecting the (value) of the (email):from its input-tag:
+        => and send that value to its (useState) hook variable:*/}
+
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       {/* here we create another div:
       => under this div. we will have our submit button: */}
-      <div className={styles.field}>
-        <button>Log In</button>
+      {/* IMP = here we are giving the (disabled) property to this (button) through its (div):
+      => and that disabled property will have a (value) in it.acc to the (loggingIn) state.which we have maintain with the help of usestate hook:  */}
+      <div className={styles.field} disabled={loggingIn}>
+        {/* IMP = here we are giving the (object) to this button:
+        => with the help of our (loggingIn) state:so that user did not trigger that button again and again: */}
+        <button>{loggingIn ? "Logging in...." : "Log In"}</button>
       </div>
     </form>
   );
