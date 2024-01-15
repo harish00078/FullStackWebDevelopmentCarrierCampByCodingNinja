75f07c428e2ddc42988a3afe1cf929bdd85d3a12
harish kumar <harishpathania150@gmail.com>
2024-01-15 19:55:51 +0530

    social app using react and backend api's

---
+++ React/MileStone9Lecture14/Major-Project/codeial-react-app/src/pages/Settings.js
@@ -0,0 +1,14 @@
+// here we are building the (setting) page:
+// through which (user) can do some changes on its profile-data:
+// IMP = we only allowed few-sections Of the (profile-structure).
+// to the (user) on which they can do some changes on it (profile-data):
+// (user) only able to do changes on the (name) and (password) section of its profile-data:
+
+// here we are creating the setting-component:
+const Settings = () =>{
+
+};
+
+// here we are exporting the setting-component:
+export default Settings;
+
