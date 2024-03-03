695b94c4ff1ca35cc95a45a19bd8820cffefd91f
harish kumar <harishpathania150@gmail.com>
2024-03-03 14:42:30 +0530

    learn about setTimeout function

---
+++ FrontEnd/MileStone6/MileStone6Lecture12/setTimeOut/index.js
@@ -0,0 +1,7 @@
+// here we study about that how the (setTimeOut) function works:
+// we study about it:with the help of example:
+console.log(1);
+setTimeout(function(){
+    console.log(2);
+},0);
+console.log(3);
