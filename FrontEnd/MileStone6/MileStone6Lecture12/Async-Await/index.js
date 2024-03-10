c279cd4d53b60bfde3f0a1a64e20092d65971574
harish kumar <harishpathania150@gmail.com>
2024-03-10 12:49:50 +0530

    study about async await

--- FrontEnd/MileStone6/MileStone6Lecture12/Async-Await/index.js
+++ FrontEnd/MileStone6/MileStone6Lecture12/Async-Await/index.js
@@ -11,15 +11,27 @@ promise.then((data) => {
 
 // IMP = there is one more simpler-way through which we can create same upper-type promise:
 // same way we can do the (reject) argument as well:
-Promise.resolve("hey we resolved").then((data) => {
-  console.log(data);
-}).catch((error)=>{
+Promise.resolve("hey we resolved")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
 // => 2 = here we are using the (Async-Await) method:
 // here we will learn about that how we can use it with the promises:and how it will help us to write promises in more clearer-style:
 // IMP => 1 = we can put (async) function infront of any (function) which we have created with the help of (async) keyword:
-async function asyncTask(){
-    return "resolved";
+// V.IMP=> 2 = Await can only be used before those functions which returns a promise. Async functions always return a promise.
+// => 3 =  here we did not have to use the (Promsie) constructor to create the (promise) object:because we know that (async) function always return the (promise) Object:
+async function asyncTask() {
+  return "resolved";
 }
+// V.IMP = if (async) function always return's the promise:then we can use the (promise) handler methods on the (async) function directly:
+asyncTask()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
