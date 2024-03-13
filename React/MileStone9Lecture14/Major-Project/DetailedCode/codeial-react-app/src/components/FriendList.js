8b21861935f500121d455c06fe5cd501ef79a864
harish kumar <harishpathania150@gmail.com>
2024-03-13 10:54:23 +0530

    udpate the code

--- React/MileStone9Lecture14/Major-Project/DetailedCode/codeial-react-app/src/components/FriendList.js
+++ React/MileStone9Lecture14/Major-Project/DetailedCode/codeial-react-app/src/components/FriendList.js
@@ -1,6 +1,7 @@
 import styles from '../styles/home.module.css';
 const FriendList = () => {
     // we will get our friendslist from the (useAuth) custom-hook:
+    // because it will have the data-related to our authenticated (user):  
     const auth = useAuth();
     return(
           <div className={styles.friendsList}>
