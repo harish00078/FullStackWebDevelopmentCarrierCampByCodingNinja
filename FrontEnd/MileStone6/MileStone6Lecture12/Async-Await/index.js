d67156ee39accce93a6a9c63fe0b0da80917dbd5
harish kumar <harishpathania150@gmail.com>
2024-03-16 13:45:28 +0530

    up

--- FrontEnd/MileStone6/MileStone6Lecture12/Async-Await/index.js
+++ FrontEnd/MileStone6/MileStone6Lecture12/Async-Await/index.js
@@ -38,15 +38,82 @@ asyncTask()
 
 // V.IMP = here we see that:how and why we can use the (await) method with in the (async) function:
 // here we have the function:which basically returns the (promise) that we have created with in it:
-function getData(){
-  return Promise.resolve('have some data');
+// V.IMP = here this function act as  a (server):
+function getData() {
+  return Promise.resolve("get the data from the server");
 }
 // here we have the async-function:which basically also return the promise:
-async function abc(){
+async function abc() {
   // IMP = here with in the (async) function:we are calling the another-function which is basically also returning us the (promsie):
   // we are calling the function through (await) method:
+  // V.V.IMP = Await statements make asynchronous calls behave exactly like synchronous calls:
+  // V.IMP = here we gonna call that server with the help of its (API):
   const data = await getData();
-  console.log(data);
+  console.log("with in aysnc-function:->", data);
+  // after getting the data from the server:with the help of its (api):we gonna return that (data) from this (async) function:
+  // so we can use the (data) in our application:
+  return data;
 }
-abc()
-console.log('1');
+
+abc().then((data) => {
+  console.log("handle async-data through promise-handler methods:-> ", data);
+});
+console.log("1");
+
+
+// V.IMP =>1 =  calling another promise with in the (async) function:without the help of (await) method:
+const promise1 = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved 1!", 100);
+    });
+  });
+const promise2 = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved 2!", 100);
+    });
+  });
+function asyncAwait() {
+  // V.IMP = if we did not call the promises:through the await-methods with in the async function:
+  // then we have to handle promises:through the (promise) handler methods:with in the async function:
+  promise1()
+    .then((result) => {
+      console.log(result);
+      return promise2();
+    })
+    .then((result) => {
+      console.log(result);
+    });
+}
+asyncAwait();
+// V.IMP => 2 =  calling another promise with in the (async) function:with the help of (await) method:
+const promise3 = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved 3!", 200);
+    });
+  });
+const promise4 = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved 4!", 200);
+    });
+  });
+async function asyncAwait1() {
+  // V.IMP = if we call the promises with the help of (await) method:
+  // then we did not have use the promise-handler methods:with in the (async) function:
+  // V.IMP = because the await method will basically get the data of that promise for us:
+  // and we can store that data by creating a variable in the async function:
+  const result1 = await promise3();
+  const result2 = await promise4();
+  // => 1 =  we can use the promise (data) with in the async-fucntion as well:
+  console.log(result1);
+  console.log(result2);
+  // => 2 = or what we can do is after getting the data:we can return that data from this async function:and use it in our application from outside this function:
+  return result1;
+}
+// here we are using that (data).In our application:and we are getting that data by using the of (promise-hanlder) method on the async-function: because it will also return us the promise:
+asyncAwait1().then((data)=>{
+  console.log(data);
+})
