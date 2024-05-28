4da446ec588f8942fec10515203b9b5020edcef5
harish kumar <harishpathania150@gmail.com>
2024-05-28 19:54:49 +0530

    updated the code

--- React/MileStone9Lecture18/Topic3-CurryingInJs/code/script.js
+++ React/MileStone9Lecture18/Topic3-CurryingInJs/code/script.js
@@ -20,6 +20,8 @@ function sum(x) {
   };
 }
 
+
+
 // and here we are using function:which we have created using the curring technique:
 // => Explanation on currying's working technique:sum is a curried function that takes one argument x and returns another function that takes one argument y, which returns a third function that takes one argument z. The final function returns the sum of x, y, and z.
 // 1 -> When sum(2) is called, it returns a new function that takes one argument y.This returned function is assigned to sumXResult.
