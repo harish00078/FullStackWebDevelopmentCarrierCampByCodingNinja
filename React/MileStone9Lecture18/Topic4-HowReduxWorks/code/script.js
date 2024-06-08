754aa1453929442a77529a65b1f24ea98de039f9
harish kumar <harishpathania150@gmail.com>
2024-06-08 20:22:50 +0530

    lear about js function types

--- React/MileStone9Lecture18/Topic4-HowReduxWorks/code/script.js
+++ React/MileStone9Lecture18/Topic4-HowReduxWorks/code/script.js
@@ -1,3 +1,6 @@
 // here we Learn about two function types of js:
 // 1. Pure functions.
 // 2. Impure functions.
+// => Pure functions:a pure function is a function that always returns the same output given the same input and has no side effects:
+// -> it does not modify any varaibles outside of its scope:
+// -> 
