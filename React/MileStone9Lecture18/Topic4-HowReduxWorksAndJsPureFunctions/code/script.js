55b3152266773c68439086d846e426f711cdf390
harish kumar <harishpathania150@gmail.com>
2024-06-09 19:57:11 +0530

    learn about diff functions in js

--- React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
+++ React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
@@ -1,6 +1,6 @@
 // here we Learn about two function types of js:
 // 1. Pure functions.
 // 2. Impure functions.
-// => Pure functions:a pure function is a function that always returns the same output given the same input and has no side effects:
+// => Pure functions:a Pure function is a function that always returns the same output as be given the same input and has no side effects:
 // -> it does not modify any varaibles outside of its scope:
 // -> 
