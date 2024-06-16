e4e09aac867ff120d29cc6690690f2ed5b14e4b8
harish kumar <harishpathania150@gmail.com>
2024-06-16 20:36:33 +0530

    learn about function types in js

--- React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
+++ React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
@@ -1,3 +1,28 @@
 // here we Learn about two function types of js:
 // 1. Pure functions:
 // 2. Impure functions:
+
+// => Pure Functions:A pure function is a function that always returns the same output as be give him the same input and has no side effects:
+// => we have two types of pure functions:
+// -> first are our own created pure functions:
+// -> second are built-in pure functions in js:
+
+// => 1 = our own created pure functions:
+// here are some examples of our created pure functions:
+function add(a,b){
+    return a+b;
+}
+function multiply(a,b){
+    return a*b;
+}
+// here we are calling our own created pure functions:
+console.log(
+    add(2,5),
+    multiply(2,5)
+)
+// => 2 = built-in pure functions in js:
+// -> Math.abs()
+// -> parseInt()
+// -> JSON.Stringify() and many others
+
+// => Impure functions:
