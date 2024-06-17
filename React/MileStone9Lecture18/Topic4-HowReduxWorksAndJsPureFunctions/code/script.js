da40cd2fad7fb17ba48da23012c42412d0139705
harish kumar <harishpathania150@gmail.com>
2024-06-17 19:02:09 +0530

    learn about diff function types of js

--- React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
+++ React/MileStone9Lecture18/Topic4-HowReduxWorksAndJsPureFunctions/code/script.js
@@ -25,4 +25,34 @@ console.log(
 // -> parseInt()
 // -> JSON.Stringify() and many others
 
-// => Impure functions:
+// => Impure functions:An Impure function is a function that either modifies variables outside of its scope,mutates its input arguments,has I/O operations as well such as reading from or writing to a file or database,or has other side effects that are not purely computational.
+// IMP = In simple words it can basically be any function that is not pure.and we call that because it can access and manipulate values of other variables with in him:
+
+// these are also two types of impure functions:
+// -> first are our own created impure functions:
+// -> second are built-in impure functions in js:
+
+// => 1 = our own created impure functions:
+// here is an example of impure function:
+const message = 'Hi';
+function myMessage(value){
+    // => we can return values from function in two ways:
+
+    // -> 1 = first is the simple one which we always use:
+    // return message + ' ' + value;
+
+    // -> 2 = second is by using template-literals which is the most efficient way:
+    return `${message} ${value}`
+}
+
+
+// here we are calling our own created impure functions:
+console.log(
+    myMessage('harish')
+)
+// => 2 = built-in impure functions in js:
+// -> Math.random()
+// -> console.log()
+// -> Date.now()
+// -> Array.sort()
+// -> Array.splice() and many others 
