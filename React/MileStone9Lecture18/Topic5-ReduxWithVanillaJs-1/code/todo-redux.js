16d70938d4f9528459292bef254f4b984d528c65
harish kumar <harishpathania150@gmail.com>
2024-06-28 20:38:28 +0530

    using redux with vanilla js

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -1,1 +1,18 @@
 // here we gonna learn about that:how we can use the redux in vanilla js:
+
+// => 1 = after installing redux we have to import it in our js file:
+const redux = require('redux');  
+
+// => 2 = after importing the redux we have to create the (actions).acc to our application:
+// for this application we only need the two actions:
+// => here we are (defining) those actions.or we can say defining there types:
+// -> add-todo:
+// -> toggle-todo:
+const ADD_TODO = 'Add TODO';
+const TOGGLE_TODO = 'Toggle TODO';
+// => here we are (creating) those actions:we can also call it (action creators) or (action creator functions).using which we are gonna create the  actions:
+// -> first we create addTodo-action:
+const addToDo = (text)=>({text:text,type:ADD_TODO});
+// ->  second we create toggleTodo-action:
+const toggleToDo = (id) =>({id:id,type:TOGGLE_TODO});
+
