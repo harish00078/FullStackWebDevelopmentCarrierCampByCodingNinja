183779691f36a6b2a4895bbce7987e502e8a832e
harish kumar <harishpathania150@gmail.com>
2024-08-06 19:54:42 +0530

    redux architecture

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -1,16 +1,24 @@
-// here we gonna learn about that:how we can use the redux in vanilla js:
+// here we gonna learn about that:how we can use the redux with vanilla-js:
 
-// => 1 = after installing redux we have to import it in our js or application ifile:
+// => 1 = after installing redux we have to import it in our js or application file:
 const redux = require('redux');  
-// => 2 = after importing the redux we need to (define) the (actions).acc to our application:
+
+// => 2 = after importing the redux we need build the (actions-section).acc to our application:
+// => [actions] section:it has basically have divided into two parts:
+// -> first is to define the (actions):
+// -> second is to create the (actions):
+
+// => here we are defining the (actions):
 // for this application we only need the two actions:
 // => here we are (defining) those actions.or we can say defining there types:
 // -> add-todo:
 // -> toggle-todo: 
 const ADD_TODO = 'Add TODO';
 const TOGGLE_TODO = 'Toggle TODO';
-// => here we are (creating) those actions:we can also call it (action creators) or (action creator functions).using which we are gonna create the  actions:
-// IMP = action-creator-functions.basically have two parameters in them:
+
+// => here we are (creating) those actions:
+// IMP = we can create the (actions) using (action creators) or (action creator functions):
+// IMP = action-creator-functions.mostly have two parameters in them:
 // -> 1 = text or id:(text) or (id) related to the todo:
 // -> 2 = type:type of the todo:or we can say that the (action-type) of the todo:
 // -> first we create addTodo-action:
