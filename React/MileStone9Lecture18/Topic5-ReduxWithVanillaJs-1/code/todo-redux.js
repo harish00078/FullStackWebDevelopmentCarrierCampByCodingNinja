ddc2fa4a35f60d8020a89d59f5b3ca4233f1411a
harish kumar <harishpathania150@gmail.com>
2024-08-13 19:38:10 +0530

    learn to use redux with vanilla-js

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -1,7 +1,7 @@
 // here we gonna learn about that:how we can use the redux with vanilla-js:
 
 // => 1 = after installing redux we have to import it in our js or application file:
-const redux = require('redux');  
+const redux = require("redux");
 
 // => 2 =[Actions-section]= after importing the redux we need build the (actions-section).acc to our application:
 // => [actions] section:it has basically have divided into two parts:
@@ -12,9 +12,9 @@ const redux = require('redux');
 // for this application we only need the two actions:
 // => here we are (defining) those actions.or we can say defining there types:
 // -> add-todo:
-// -> toggle-todo: 
-const ADD_TODO = 'Add TODO';
-const TOGGLE_TODO = 'Toggle TODO';
+// -> toggle-todo:
+const ADD_TODO = "Add TODO";
+const TOGGLE_TODO = "Toggle TODO";
 
 // => here we are (creating) those actions:
 // IMP = we can create the (actions) using (action creators) or (action creator functions):
@@ -23,16 +23,22 @@ const TOGGLE_TODO = 'Toggle TODO';
 // -> 2 = type:type of the todo:or we can say that the (action-type) of the todo:
 
 // -> first we create addTodo-action:
-const addToDo = (text)=>({text:text,type:ADD_TODO});
+const addToDo = (text) => ({ text: text, type: ADD_TODO });
 
 // ->  second we create toggleTodo-action:
-const toggleToDo = (id) =>({id:id,type:TOGGLE_TODO});
+const toggleToDo = (id) => ({ id: id, type: TOGGLE_TODO });
 
 // => 3 = [Reducers-section]:reducers are used to change the state of the application on the store:they are basically the (pure-functions):
 // IMP = reducers basically have the (state) and (action) as an argument in them:and it will return the new state or the updated state:
-// => here we are creating the (reducer):by defining the (todoReducer) pure-function:
-function todoReducer(state,action){
-    // IMP = before performing any action on the state we need to check the action-type:for doing that we gonna be use the (switch-case) statement:
+// => here we are defining the (reducer):by creating the (todoReducer) pure-function:
+function todoReducer(state, action) {
+  // IMP = before performing any action on the state we need to check the action-type:for doing that we gonna be use the (switch-case) statement:
+  switch (action.type) {
+    case ADD_TODO:
+    case TOGGLE_TODO:
+    default:
+      return state;
+  }
 
-    // here we are creating the (default) state:
+  // here we are creating the (default) state:
 }
