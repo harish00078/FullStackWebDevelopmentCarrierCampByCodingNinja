c2915bb83242de2562f334fbe4ff69c608c137f8
harish kumar <harishpathania150@gmail.com>
2024-09-22 19:30:26 +0530

    update

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -35,10 +35,10 @@ const toggleToDo = (id) => ({ id: id, type: TOGGLE_TODO });
 // => 4 = [Initial state]: here we are defining the (initial state) of the application:because when we are creating the redux-architecture (store) for application we need to define the initial state with in it as well:
 const initialState = {
   todos: [],
-};  
- 
+};
+
 // => 3 = [Reducers-section]:reducers are used to change the state of the application on the store:they are basically the (pure-functions):
-// IMP = reducers basically have the (state) and (action) as an argument in them:and it will return the new state or the updated state:
+// IMP = reducers basically have the (current-state) and (action) as an argument in them:and it will return the new state or the updated state:
 // V.IMP-NOTE = In reducers we have to define the (default) state:and that is gonna be our (initial-state):
 // => here we are defining the (reducer):by creating the (todoReducer) pure-function:
 function todoReducer(state = initialState, action) {
