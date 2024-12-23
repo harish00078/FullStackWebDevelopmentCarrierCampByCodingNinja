3a6748fbacf366f90047645b8c9e59a045269d83
harish kumar <harishpathania150@gmail.com>
2024-12-23 19:04:45 +0530

    updated

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -49,7 +49,7 @@ function todoReducer(state = initialState, action) {
     // V.IMP = then we will return a new object which will become our new application state:it will contain the old state of the application which we have spread in it: with the help of the spread operator:
     // V.IMP = and we will also add or update the specific property of the current state within the new object or new state:which is related to the action type:
     case ADD_TODO:
-      // IMP = here we are returing the new-state:
+      // IMP = here we are returning the new-state:
       return {
         // here we are basically updating the (todos) property of the (state) object:
         // IMP = we are basically adding the new-todo with old-todos:in the todos-property of the state-object:
