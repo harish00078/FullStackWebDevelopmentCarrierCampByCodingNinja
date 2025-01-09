39a97aa41ed15ab46527db59d90c162eaa81b9b5
harish kumar <harishpathania150@gmail.com>
2025-01-09 16:35:56 +0530

    learn to read data from console

--- React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
+++ React/MileStone9Lecture18/Topic5-ReduxWithVanillaJs-1/code/todo-redux.js
@@ -1,4 +1,4 @@
-console.log("Learn about redux with vanilla js");
+console.log("Learn To Use redux with vanilla js");
 // here we gonna learn about that:how we can use the redux with vanilla or simple js:
 
 // => 1 = after installing redux we have to import it in our js or application file:
