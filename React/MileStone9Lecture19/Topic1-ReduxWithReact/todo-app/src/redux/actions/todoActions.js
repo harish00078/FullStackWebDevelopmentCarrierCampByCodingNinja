ca94d3df2c7edcc6e4ef7a1a3585040338e66681
harish kumar <harishpathania150@gmail.com>
2025-02-07 19:15:27 +0530

    learning about using redux with react

---
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/actions/todoActions.js
@@ -0,0 +1,4 @@
+// Creating Actions:
+// => As we have learned:that in redux-architecture's code action-section have two parts:
+// -> first is to define the (actions or action-types):
+// -> second is to create the those (actions):using (action-creator-functions):
