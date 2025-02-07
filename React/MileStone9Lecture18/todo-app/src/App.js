3561e9c1397133fe6dc776a3019b4067c41baf80
harish kumar <harishpathania150@gmail.com>
2025-02-07 10:55:07 +0530

    learning about using redux with react

--- React/MileStone9Lecture18/todo-app/src/App.js
+++ React/MileStone9Lecture18/todo-app/src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Todo App</h1>
-      
+      <TodoForm onCreateTodo={createTodo}/>
+      <TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   );
 }
