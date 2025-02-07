fc8ee08f3d5ee6dbb1330b88d1eae2d35dcd7c61
harish kumar <harishpathania150@gmail.com>
2025-02-07 12:29:27 +0530

    updated directory structure

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import "./App.css";
 import TodoForm from "./components/ToDoForm/TodoForm";
 import TodoList from "./components/ToDoList/TodoList";
+
+import './App.css';
+
 function App() {
   const [todos, setTodos] = useState([]);
-  // createTodo function:
+
   const createTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+    setTodos([...todos, { id: todos.length + 1, text, completed: false}]);
   };
-  // toggleTodo Function:
+
   const toggleTodo = (index)=>{
     const list = [...todos];
     list[index].completed = !list[index].completed;
@@ -17,7 +19,8 @@ function App() {
 
   return (
     <div>
-      <h1>Todo App</h1>
+      <h1>To Do App</h1>
+
       <TodoForm onCreateTodo={createTodo} />
       <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
