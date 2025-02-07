c8d1c42912eac909803f43c5299eda3c20d1f9c6
harish kumar <harishpathania150@gmail.com>
2025-02-07 11:06:03 +0530

    learning about using redux with react

--- React/MileStone9Lecture18/todo-app/src/App.js
+++ React/MileStone9Lecture18/todo-app/src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
-
+import TodoForm from "./components/ToDoForm/TodoForm";
+import TodoList from "./components/ToDoList/TodoList";
 function App() {
+  const [todos, setTodos] = useState([]);
+  // createTodo function:
+  const createTodo = (text) => {
+    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+  };
+  // toggleTodo Function:
+
   return (
     <div>
       <h1>Todo App</h1>
-      <TodoForm onCreateTodo={createTodo}/>
-      <TodoList todos={todos} onToggle={toggleTodo}/>
+      <TodoForm onCreateTodo={createTodo} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
---
+++ React/MileStone9Lecture18/todo-app/src/components/ToDoForm/TodoForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./TodoForm.css";
+function TodoForm() {
+  const [todoText, setTodoText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreateTodo(todoText);
+    setTodoText("");
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        className="form-control mb-3"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button className="btn btn-success float-end" type="submit">
+            Create Todo
+        </button>
+      </form>
+    </div>
+  );
+}
+export default TodoForm;
