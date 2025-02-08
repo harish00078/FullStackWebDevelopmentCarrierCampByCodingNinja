192c4bc3b4a57185e4662b53598843220321be69
harish kumar <harishpathania150@gmail.com>
2025-02-08 18:38:48 +0530

    learning about operators in js

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
@@ -1,7 +1,7 @@
 // Creating Reducers:For (todo) for application:
 // -> As we have learned that:Reducers are the (pure) functions:which basically takes (two-arguments) with in them:
 // -> First:is (current-state) of the application:
-// -> Second:is the (action) which we have perform on the Current-state and get the new or updated state from it:
+// -> Second:is the (action) which we have to perform on the Current-state and get the new or updated state from it:
 
 function todoReducer(state,action){
     // here we gonna be update the state:acc to the action-type:
