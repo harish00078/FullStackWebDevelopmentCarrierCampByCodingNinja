f4198d8cb0b097a1c9b7f9d4591c60d09c9ad592
harish kumar <harishpathania150@gmail.com>
2025-02-09 11:27:12 +0530

    learning about operators in js

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
@@ -1,3 +1,7 @@
+// Importing the Actions or Action-Creator-Functions:which we have created by our selfs:
+import {ADD_TODO,TOGGLE_TODO} from "../actions/todoActions"
+
+
 // Creating Reducers:For (todo) for application:
 // -> As we have learned that:Reducers are the (pure) functions:which basically takes (two-arguments) with in them:
 // -> First:is (current-state) of the application:
@@ -7,9 +11,12 @@ function todoReducer(state,action){
     // here we gonna be update the state:acc to the action-type:
     // IMP: for checking actions or action-types and returning our updated state acc to them: we are going to use the (switch and case) statement-method:
 
-    // -> first we check the action-type:
+   
     switch(action.type){
-        // rather than action-types:if we did not get any action-type:then we will return the (default) state or we can say the  of the which we got be also have to return the default
+         // -> first we check the action-type:with the help of (case) method of (switch and case) statement:
+         case ADD_TODO:
+        // rather than action-types:if we did not get or have any action-type:then we will return the (default) state or we can say the (Current) state of the application:
+       
     }
 
 }
