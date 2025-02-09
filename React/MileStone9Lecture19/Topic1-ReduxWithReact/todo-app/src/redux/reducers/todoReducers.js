6c6607141ff2ba08b52fbc756d02193414bd9788
harish kumar <harishpathania150@gmail.com>
2025-02-09 16:40:55 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/todoReducers.js
@@ -1,7 +1,6 @@
 // Importing the Actions or Action-Creator-Functions:which we have created by our selfs:
 import {ADD_TODO,TOGGLE_TODO} from "../actions/todoActions"
 
-
 // Creating Reducers:For (todo) for application:
 // -> As we have learned that:Reducers are the (pure) functions:which basically takes (two-arguments) with in them:
 // -> First:is (current-state) of the application:
@@ -11,7 +10,6 @@ function todoReducer(state,action){
     // here we gonna be update the state:acc to the action-type:
     // IMP: for checking actions or action-types and returning our updated state acc to them: we are going to use the (switch and case) statement-method:
 
-   
     switch(action.type){
          // -> first we check the action-type:with the help of (case) method of (switch and case) statement:
          case ADD_TODO:
