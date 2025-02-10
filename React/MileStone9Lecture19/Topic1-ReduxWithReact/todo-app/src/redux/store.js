dbf53bbd30e572dd82999491a95b8fbe65deb95b
harish kumar <harishpathania150@gmail.com>
2025-02-10 10:20:33 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
@@ -4,4 +4,7 @@ import redux from "redux";
 // second: we have to import the (reducer):through which we are gonna be able to create or manage our store:
 import { todoReducer } from "./reducers/todoReducers";
 
-// third: here we creat eht 
+// third: here we create the store:
+// and we also have to export the store:so that we can interact or use it with in our application:
+export const store = redux.createStore(todoReducer);
+
