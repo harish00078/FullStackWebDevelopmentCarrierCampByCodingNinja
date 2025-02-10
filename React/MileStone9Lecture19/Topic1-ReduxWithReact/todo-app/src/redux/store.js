ec3f4884aa1b7d62168598d9756385852e4b1639
harish kumar <harishpathania150@gmail.com>
2025-02-10 10:03:00 +0530

    learning about using redux with react

---
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
@@ -0,0 +1,7 @@
+// first: we import the (redux):with the help of (ES6) syntax:
+import redux from "redux";
+
+// second: we have to import the (reducer):through which we are gonna be able to create or manage our store:
+import { todoReducer } from "./reducers/todoReducers";
+
+// third: here we creat eht 
