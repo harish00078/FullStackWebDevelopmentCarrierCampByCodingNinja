6de1a9e3f1c103d46449f6480297c6a94b139c67
harish kumar <harishpathania150@gmail.com>
2025-02-11 10:54:44 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
@@ -1,25 +1,31 @@
 import { useState } from "react";
+// Importing the (provider):
+import { Provider } from "react-redux";
 import TodoForm from "./components/ToDoForm/TodoForm";
 import TodoList from "./components/ToDoList/TodoList";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  
+
   const createTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false}]);
+    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
   };
 
-  const toggleTodo = (index)=>{
+  const toggleTodo = (index) => {
     const list = [...todos];
     list[index].completed = !list[index].completed;
     setTodos(list);
-  }
+  };
 
   return (
     <div>
       <h1>To Do App</h1>
+      {/* IMP-NOTE:we have two ways of creating and using (redux-store):
+      // V.IMP:The Provider component takes a (store-prop), which is the Redux store that you want to provide to your application
+      -> 1-way: create store in the form of (global-redux-store):and then we can use it with the specific or all the components of the application:with the help of (react-redux) libraries (provider-component):
+      -> 2-way: create store in the form of (component-redux-store):we call it component-store.because here we create and use the (specific-store) for the (specific-component):and provide that specific-store to the specific-component:with the help of (provider-component) as well:by passing the specific-store to the store-prop */}
 
       <TodoForm onCreateTodo={createTodo} />
       <TodoList todos={todos} onToggle={toggleTodo} />
