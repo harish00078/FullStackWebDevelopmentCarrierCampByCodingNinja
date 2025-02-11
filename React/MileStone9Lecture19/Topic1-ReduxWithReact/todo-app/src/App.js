408dfebb23f21232af789ab98db261ad27bffad1
harish kumar <harishpathania150@gmail.com>
2025-02-11 11:01:03 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
@@ -23,12 +23,17 @@ function App() {
     <div>
       <h1>To Do App</h1>
       {/* IMP-NOTE:we have two ways of creating and using (redux-store):
-      // V.IMP:The Provider component takes a (store-prop), which is the Redux store that you want to provide to your application
+      // V.IMP:The Provider component takes a (store-prop), which is the Redux store that you want to provide to your application:
+      // V.IMP: the use of multiple stores is not recommended:
+
       -> 1-way: create store in the form of (global-redux-store):and then we can use it with the specific or all the components of the application:with the help of (react-redux) libraries (provider-component):
       -> 2-way: create store in the form of (component-redux-store):we call it component-store.because here we create and use the (specific-store) for the (specific-component):and provide that specific-store to the specific-component:with the help of (provider-component) as well:by passing the specific-store to the store-prop */}
 
-      <TodoForm onCreateTodo={createTodo} />
-      <TodoList todos={todos} onToggle={toggleTodo} />
+      {/* Here we using the first-way to create and use store: */}
+      <Provider>
+        <TodoForm onCreateTodo={createTodo} />
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </Provider>
     </div>
   );
 }
