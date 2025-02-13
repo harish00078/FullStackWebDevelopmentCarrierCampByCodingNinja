3d7c2d35a38f437663f4b9f10d757972f35ac07d
harish kumar <harishpathania150@gmail.com>
2025-02-13 21:04:45 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoList/TodoList.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoList/TodoList.js
@@ -29,19 +29,20 @@ import "./TodoList.css";
 // IMP: To have data with in the component, we use the Redux store, which holds the application's state:we can say that the (redux-store) is the (state):
 // IMP: The React-Redux library provides built-in custom-hooks that allow us to access and interact with the store or state efficiently.
 
-// Key-Point: Accessing Data with useSelector
-// -> The useSelector hook, provided by React-Redux, enables components to access the Redux store without needing to import the store directly in each component.
-// -> It allows us to retrieve specific-sections of the state instead of the entire store.
-// -> Additionally, it ensures automatic updates, meaning whenever the selected-part of the state-changes, the component re-renders automatically to reflect the latest-data.
+// Key-Point: Accessing Data with useSelector-hook:
+// -> The useSelector-hook, provided by React-Redux, enables components to access the Redux store without needing to import the store directly in each component.
+// -> It allows us to retrieve specific-sections of the state instead of the entire-store.
+// -> Additionally, it ensures automatic-updates, meaning whenever the selected-part of the state-changes, the component re-renders automatically to reflect the latest-data.
 
 // => working or usage of useSelector-hook:
 // -> Import it from the react-redux library:
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 // -> then use it to get the (store) or (state):which has been provide to us by the (provided-component) of the (react-redux) library as well:
 // -> useSelector-hook takes a function or selector-function as an argument.
 // -> The function-receives the entire Redux-store-state and returns the specific part of the state you need or which we basically define with in the function:
 
 function ToDoList({todos, onToggle}) {
+  const 
   return (
     <div className="container">
     <ul>
