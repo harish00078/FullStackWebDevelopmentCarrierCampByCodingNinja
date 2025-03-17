b47cf80e298087ae345f09e108c01c32cfeb2ef9
harish kumar <harishpathania150@gmail.com>
2025-03-17 17:50:31 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoForm/TodoForm.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoForm/TodoForm.js
@@ -1,29 +1,33 @@
 import { useState } from "react";
 import "./TodoForm.css";
 
-function ToDoForm({ onCreateTodo }) {
-  const [todoText, setTodoText] = useState("");
+// Here we are handling data in simple-way:with the help of (prop) method:
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onCreateTodo(todoText);
-    setTodoText("");
-  };
+// function ToDoForm({ onCreateTodo }) {
+//   const [todoText, setTodoText] = useState("");
 
-  return (
-    <div className="container">
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     onCreateTodo(todoText);
+//     setTodoText("");
+//   };
+
+//   return (
+//     <div className="container">
       
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="form-control mb-3"
-        value={todoText}
-        onChange={(e) => setTodoText(e.target.value)}
-      />
-      <button className="btn btn-success float-end" type="submit">Create Todo</button>
-    </form>
-    </div>
-  );
-}
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         className="form-control mb-3"
+//         value={todoText}
+//         onChange={(e) => setTodoText(e.target.value)}
+//       />
+//       <button className="btn btn-success float-end" type="submit">Create Todo</button>
+//     </form>
+//     </div>
+//   );
+// }
 
-export default ToDoForm;
+// export default ToDoForm;
+
+
