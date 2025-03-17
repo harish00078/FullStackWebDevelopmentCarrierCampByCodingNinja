389be665e7e83e20e1c3261b558b7f36c16841b4
harish kumar <harishpathania150@gmail.com>
2025-03-17 17:51:35 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoForm/TodoForm.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/ToDoForm/TodoForm.js
@@ -31,3 +31,30 @@ import "./TodoForm.css";
 // export default ToDoForm;
 
 
+// Here we are handling the help of state-management-library:which is the redux-library:
+function ToDoForm({ onCreateTodo }) {
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreateTodo(todoText);
+    setTodoText("");
+  };
+
+  return (
+    <div className="container">
+      
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-control mb-3"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+      <button className="btn btn-success float-end" type="submit">Create Todo</button>
+    </form>
+    </div>
+  );
+}
+
+export default ToDoForm;
