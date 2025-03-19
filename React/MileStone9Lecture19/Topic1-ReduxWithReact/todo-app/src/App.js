e6fb2544dd045d67abc02a3c313c67d7feffa24d
harish kumar <harishpathania150@gmail.com>
2025-03-19 10:56:40 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
@@ -8,17 +8,17 @@ import {store} from './redux/store';
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
 
-  const createTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
-  };
+  // const createTodo = (text) => {
+  //   setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+  // };
 
-  const toggleTodo = (index) => {
-    const list = [...todos];
-    list[index].completed = !list[index].completed;
-    setTodos(list);
-  };
+  // const toggleTodo = (index) => {
+  //   const list = [...todos];
+  //   list[index].completed = !list[index].completed;
+  //   setTodos(list);
+  // };
 
   return (
     <div>
