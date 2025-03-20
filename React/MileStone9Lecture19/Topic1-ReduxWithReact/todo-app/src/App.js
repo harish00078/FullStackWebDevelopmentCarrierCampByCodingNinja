6f2da4756a8aa1952a12591769907cdb9c09a59a
harish kumar <harishpathania150@gmail.com>
2025-03-20 17:58:54 +0530

    up

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/App.js
@@ -51,7 +51,7 @@ function App() {
             {/* Understanding <Fragment>:
             => 1: <Fragment> is a React component that allows you to group multiple elements without adding an unnecessary wrapper element (like <div>).
             => 2: This is useful when returning multiple components inside a single element prop of <Route>. */}
-            <Route path="" element={<Home />}></Route>
+            <Route path="" element={<Home />}/>
 
             <Route
               path="todo"
