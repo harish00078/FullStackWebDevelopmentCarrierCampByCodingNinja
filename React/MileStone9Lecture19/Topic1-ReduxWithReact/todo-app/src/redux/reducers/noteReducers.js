d8aae3a15281de090354094b7cb3cccfd82b5e82
harish kumar <harishpathania150@gmail.com>
2025-03-20 17:08:18 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/noteReducers.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/noteReducers.js
@@ -1,1 +1,11 @@
+// Import Actions:
+import { ADD_NOTE, DELETE_NOTE } from "../actions/noteActions";
 
+// Defining Initial-State:
+const initialState = {
+     notes :[
+        {text: "Finish React project",createdOn: new Date() },
+        {  text: "Review Redux concepts" ,createdOn: new Date()},
+        {  text: "Prepare for coding interview",createdOn: new Date() }
+    ],
+};
