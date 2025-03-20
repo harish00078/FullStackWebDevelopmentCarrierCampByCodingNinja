2d012bba4c135106d776c443854bd94317dcb878
harish kumar <harishpathania150@gmail.com>
2025-03-20 17:02:43 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/actions/noteActions.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/actions/noteActions.js
@@ -1,5 +1,13 @@
 // Defining Action For Notes-Section:
-const ADD_NOTE='Add Note';
-const DELETE_NOTE='Delete Note';
+const ADD_NOTE = "Add Note";
+const DELETE_NOTE = "Delete Note";
 
-// Action-Creator-Functions:
+// Action-Creator-Functions For Creating Actions Or Action-Objects:
+export const addNote = (text) => ({
+  text: text,
+  type: ADD_NOTE,
+});
+export const deleteNote = (id) => ({
+    id: id,
+    type: DELETE_NOTE,
+})
