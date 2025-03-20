1abe15db91f455ea6e09152c0f5fd06505de00b1
harish kumar <harishpathania150@gmail.com>
2025-03-20 17:13:34 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/noteReducers.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/reducers/noteReducers.js
@@ -3,9 +3,29 @@ import { ADD_NOTE, DELETE_NOTE } from "../actions/noteActions";
 
 // Defining Initial-State:
 const initialState = {
-     notes :[
-        {text: "Finish React project",createdOn: new Date() },
-        {  text: "Review Redux concepts" ,createdOn: new Date()},
-        {  text: "Prepare for coding interview",createdOn: new Date() }
-    ],
+  notes: [
+    { text: "Finish React project", createdOn: new Date() },
+    { text: "Review Redux concepts", createdOn: new Date() },
+    { text: "Prepare for coding interview", createdOn: new Date() },
+  ],
 };
+
+export function noteReducer(state = initialState, action) {
+  // -> Using the Switch-case statement:
+  switch (action.type) {
+    case ADD_NOTE:
+      return {
+        ...state,
+        notes: [
+          ...state.notes,
+          {
+            text: action.text,
+            createdOn: new Date(),
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+}
