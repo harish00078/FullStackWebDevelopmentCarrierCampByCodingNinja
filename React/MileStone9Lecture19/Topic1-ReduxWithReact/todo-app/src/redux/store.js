d73b42381b6059912ac9227314145639746ba764
harish kumar <harishpathania150@gmail.com>
2025-03-21 19:35:01 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
@@ -5,6 +5,12 @@ import * as redux from "redux";
 // second: we have to import the (reducer):through which we are gonna be able to create or manage our store:
 import { todoReducer } from "./reducers/todoReducers";
 
+
+// Handling-Multiple-Reducers:
+// For handling multiple reducers:we have to use the (combineReducers) method:which is provided by the (redux) library:
+// -> combineReducers method is used to combine multiple reducers into a single reducer:
+// -> combineReducers method takes an object as an argument:where we pass the reducers as key-value pairs: 
+
 // third: here we create the store:
 // and we also have to export the store:so that we can interact or use it with in our application:
 export const store = redux.createStore(todoReducer);
