5ff880a9405d709d493c10b23a36bf1668ef41ea
harish kumar <harishpathania150@gmail.com>
2025-03-21 20:35:21 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/store.js
@@ -2,14 +2,33 @@
 // IMP-NOTE:we can import redux directly with in the (react) components:we have to import it as or with the (*) keyword:which means in the react that we are importing everything related to this library:
 import * as redux from "redux";
 
+// Importing the (combineReducers) method from the (redux) library:which basically help us to handle multiple reducers in the redux:
+import { combineReducers } from "redux";
+
 // second: we have to import the (reducer):through which we are gonna be able to create or manage our store:
+// => TodoReducer:
 import { todoReducer } from "./reducers/todoReducers";
+// => NoteReducer:
+import { noteReducer } from "./reducers/noteReducers";
 
 
-// Handling-Multiple-Reducers:
-// For handling multiple reducers:we have to use the (combineReducers) method:which is provided by the (redux) library:
-// -> combineReducers method is used to combine multiple reducers into a single reducer:
-// -> combineReducers method takes an object as an argument:where we pass the reducers as key-value pairs: 
+// => Handling Multiple Reducers in Redux
+// To manage multiple reducers efficiently, we use the combineReducers method provided by the Redux library.
+
+// -> Using combineReducers:
+// It allows us to merge multiple reducers into a single root or head reducer.
+// This root or head reducer is then passed to the createStore method to create the Redux-store.
+
+// -> Structure of combineReducers:
+// It takes an object as an argument.
+// -> IMP: Keys represent different sections or parts of the state.
+// -> IMP: Values are the respective reducers that manage those parts of the state.
+
+
+
+
+
+
 
 // third: here we create the store:
 // and we also have to export the store:so that we can interact or use it with in our application:
