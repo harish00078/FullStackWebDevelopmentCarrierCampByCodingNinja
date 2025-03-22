347e0c43069e5da3a71eba53eefb9a8331e7230b
harish kumar <harishpathania150@gmail.com>
2025-03-22 17:40:00 +0530

    learning about using redux with react

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/actions/noteActions.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/redux/actions/noteActions.js
@@ -1,8 +1,8 @@
 // Defining Action For Notes-Section:
 
 // V.IMP-NOTE:One Important thing here we have is that we have to (export) these all:so that we can use or connect them with the (reducer) or other-sections of the redux-architecture:because here we not implementing the hole redux-architecture with in the one-file:
-const ADD_NOTE = "Add Note";
-const DELETE_NOTE = "Delete Note";
+export const ADD_NOTE = "Add Note";
+export const DELETE_NOTE = "Delete Note";
 
 // Action-Creator-Functions For Creating Actions Or Action-Objects:
 export const addNote = (text) => ({
