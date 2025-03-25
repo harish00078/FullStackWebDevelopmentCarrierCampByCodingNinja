92f4c076ea76eb2780eb31f9c5bffbb261993758
harish kumar <harishpathania150@gmail.com>
2025-03-25 11:24:33 +0530

    up

--- React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/NavBar/NavBar.js
+++ React/MileStone9Lecture19/Topic1-ReduxWithReact/todo-app/src/components/NavBar/NavBar.js
@@ -1,13 +1,13 @@
 import "./NavBar.css";
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
           Utility Apps
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -15,17 +15,17 @@ function NavBar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="todo">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="todo">
                 To Do
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="notes">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="notes">
                 Note
               </a>
             </li>
