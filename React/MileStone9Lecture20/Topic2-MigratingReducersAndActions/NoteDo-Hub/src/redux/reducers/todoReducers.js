f3be0cdd325b950958cddfb2142365393a995f54
harish kumar <harishpathania150@gmail.com>
2025-03-29 17:53:21 +0530

    learning about migrating from redux to redux-toolkit

--- React/MileStone9Lecture20/Topic2-MigratingReducersAndActions/NoteDo-Hub/src/redux/reducers/todoReducers.js
+++ React/MileStone9Lecture20/Topic2-MigratingReducersAndActions/NoteDo-Hub/src/redux/reducers/todoReducers.js
@@ -18,8 +18,13 @@ const initialState = {
 // -> The createSlice function in Redux Toolkit defines a specific section of the application's state, similar to how we defined state in regular Redux reducers:but it does that in more efficient or simpler way:
 // -> It does that by simply combining both reducers and actions into a single configuration for a particular feature or section and slice or part of the application or application-state:
 
+// How We Gonna Do That:
+// -> First-Step:
+// -> We have to import the (createSlice) function from the redux-toolkit:
 
-
+// -> Second-Step:
+// -> we have to call or invoke the (createSlice) function:and store the result in a constant or variable:so that we can use it in our application by exporting it:
+// IMP: we gonna name it as per our application or feature or section and slice or part of the application:
 
 
 
